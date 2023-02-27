c00e1a9e9dba2abd2a324d3039fdae161dbd3baa
İbrahim Rauf Alper <119485615+ibrahimrauf@users.noreply.github.com>
2023-02-27 20:27:56 +0300

    Add files via upload

---
+++ JS fontsize ve renk/js/site.js
@@ -0,0 +1,34 @@
+var btn1=document.querySelector("#btn1");
+
+btn1.onclick=function () {
+    txt1.className="text-center m-5 text-success"
+    
+}
+btn2.onclick=function () {
+    txt1.className="text-center m-5 text-secondary"
+    
+}
+btn3.onclick=function () {
+    txt1.className="text-center m-5 text-info"
+    
+}
+btn4.onclick=function () {
+    txt1.className="text-center m-5 text-danger"
+    
+}
+btn5.onclick=function () {
+    txt1.className="text-center m-5 text-primary"
+    
+}
+btn6.onclick=function () {
+    txt1.className="text-center m-5 text-light"
+    
+}
+btn7.onclick=function () {
+    txt1.className="text-center m-5 text-dark"
+    
+}
+btn8.onclick=function () {
+    txt1.className="text-center m-5 text-warning"
+    
+}
